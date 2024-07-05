a7a9e572b07e7adce633ca8a773d9cdb0ac91e8f
Roble✨ <ritikagarwal570@gmail.com>
2024-07-05 20:30:00 +0530

    connection

--- main.go
+++ main.go
@@ -1,63 +1,18 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
+	"database/sql"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
+	"log"
+
+	_ "github.com/lib/pq"
 )
 
-const (
-	supabaseUrl = ""
-	apiKey      = ""
-)
 func main() {
-
-	if supabaseUrl == "" || apiKey == "" {
-        fmt.Println("Supabase URL or API key is missing")
-        os.Exit(1)
-    }
-	// Example data to insert
-	data := map[string]interface{}{
-		"name":  "John Doe",
-		"email": "john.doe@example.com",
-	}
-
-	jsonData, err := json.Marshal(data)
+	var url = "user=postgres.uukyjvzomuaxwnfjgatu password=0VwLOEtc4ZMZ65LM host=aws-0-us-east-1.pooler.supabase.com port=6543 dbname=postgres"
+	_, err := sql.Open("postgres", url)
 	if err != nil {
-		fmt.Println("Error marshaling JSON:", err)
-		return
+		log.Fatal(err)
 	}
-
-	// Make a POST request to insert data into the "users" table
-	url := supabaseUrl + "/rest/v1/users"
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("apikey", apiKey)
-	req.Header.Set("Authorization", "Bearer "+apiKey)
-	req.Header.Set("Prefer", "return=representation")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return
-	}
-
-	fmt.Println("Response Status:", resp.Status)
-	fmt.Println("Response Body:", string(body))
+	fmt.Print("Connected to the database\n")
 }
